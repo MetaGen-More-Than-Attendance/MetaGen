a568f1c348ba624b77344653a2b0aef04039b7cd
stoylan <toylansamet@gmail.com>
2022-02-27 23:17:05 +0300

    initial commit

---
+++ backend/src/main/java/com/hst/metagen/MetagenApplication.java
@@ -0,0 +1,13 @@
+package com.hst.metagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetagenApplication.class, args);
+	}
+
+}
