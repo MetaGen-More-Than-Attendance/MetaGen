b720c2bbe60f4c4a2ebad6a9cd692e4d0c108d54
bayogluteoman <bayogluteoman@gmail.com>
2022-03-20 20:14:38 +0300

    create methods for buttons

--- mobile/metaGen/src/pages/ScanQrScreen.js
+++ mobile/metaGen/src/pages/ScanQrScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, Image, View, Button, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as FaceDetector from 'expo-face-detector';
+import { Ionicons } from '@expo/vector-icons';
 
 const ScanQrScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
@@ -10,6 +11,7 @@ const ScanQrScreen = () => {
   const [image, setImage] = useState(null);
   const [faces, setFaces] = useState([])
   const [flash, setFlash] = useState(false)
+  const [flashIcon, setFlashIcon] = useState("md-flash-off")
 
   const handleFacesDetected = ({ faces }) => {
     setFaces(faces);
@@ -40,6 +42,22 @@ const ScanQrScreen = () => {
     console.log(image);
   }
 
+  const handleFlashMode = async () => {
+    setFlash(
+      flash === Camera.Constants.FlashMode.off
+        ? Camera.Constants.FlashMode.torch
+        : Camera.Constants.FlashMode.off
+    )
+  }
+
+  const handleReturnCamera = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <View style={styles.cameraContainer}>
@@ -57,41 +75,38 @@ const ScanQrScreen = () => {
             minDetectionInterval: 1000,
             tracking: false,
           }}
-        />
+        >
+
+          <TouchableOpacity
+            style={{
+              width: 50,
+              marginTop: 10,
+              marginLeft: 10
+            }}>
+            <Ionicons name='ios-refresh-circle' size={50} onPress={() => handleReturnCamera()} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              marginLeft: 330,
+              marginTop: 10
+            }}
+          >
+            <Ionicons name={flashIcon} size={50} onPress={() => handleFlashMode()} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              marginTop: 650,
+              marginLeft: 175,
+            }}>
+            <Ionicons name="radio-button-on-outline" size={80} onPress={() => takePicture()} />
+          </TouchableOpacity>
+
+        </Camera>
       </View>
-      <TouchableOpacity
-        style={{
-          height: 40,
-          borderRadius: 10,
-          marginBottom: 10
-        }}>
-        <Button
-          title="Flip Camera"
-          color='tomato'
-          onPress={() => {
-            setType(
-              type === Camera.Constants.Type.back
-                ? Camera.Constants.Type.front
-                : Camera.Constants.Type.back
-            );
-          }}>
-        </Button>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          height: 40,
-          borderRadius: 10,
-        }}>
-        <Button title="Take Picture" color='tomato' style={styles.button} onPress={() => takePicture()} />
-      </TouchableOpacity>
-      <Button title="Open Flash" color='tomato' style={styles.button} onPress={() => {
-        setFlash(
-          flash === Camera.Constants.FlashMode.off
-            ? Camera.Constants.FlashMode.torch
-            : Camera.Constants.FlashMode.off
-        )
-      }} />
-      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
     </View>
   )
 }
@@ -101,7 +116,6 @@ export default ScanQrScreen
 const styles = StyleSheet.create({
   cameraContainer: {
     flex: 1,
-    flexDirection: 'row'
   },
   fixedRatio: {
     flex: 1,
