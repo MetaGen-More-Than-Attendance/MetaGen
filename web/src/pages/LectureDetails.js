8eff68c220e4e8ef700f76f949f858792e74789c
bayogluteoman <bayogluteoman@gmail.com>
2022-03-31 23:00:57 +0300

    add side menu and  routers in Lecture Details page

--- web/src/pages/LectureDetails.js
+++ web/src/pages/LectureDetails.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Container, Row, Col } from 'react-bootstrap'
+import SideMenu from '../components/SideMenu';
 
 const LectureDetails = () => {
-  return (
-    <div>LectureDetails</div>
-  )
+
+    return (
+        <div style={{height:"80vh"}}>
+            <SideMenu />         
+        </div>
+    )
 }
 
 export default LectureDetails
