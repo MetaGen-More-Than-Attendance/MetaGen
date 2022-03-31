4e7f00bdb9edff263d01b632109059456000a369
bayogluteoman <bayogluteoman@gmail.com>
2022-03-31 15:43:26 +0300

    fix header

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -4,19 +4,19 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect bg="dark" expand="lg" variant="dark" className='mb-3'>
+        <Navbar collapseOnSelect bg="dark" expand="lg" style={{ backgroundColor: "#222831"}} className='mb-3'>
             <Container>
-                <Navbar.Brand >MetaGen</Navbar.Brand>
+                <Navbar.Brand style={{color: "#EEEEEE"}} >LOGO</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/lectures">Lectures</Nav.Link>
+                    <Nav className="me-auto" >
+                        <Nav.Link as={NavLink} to="/" style={{color: "#EEEEEE"}}>Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/lectures" style={{color: "#EEEEEE"}}>Lectures</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link as={NavLink} to="/login" >Login</Nav.Link>
-                        <Nav.Link as={NavLink} to="/signup" >Sign up</Nav.Link>
-                        <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
+                        <Nav.Link as={NavLink} to="/login" style={{color: "#000", backgroundColor : "#EEE", borderRadius: 10, marginRight: 5}}>Login</Nav.Link>
+                        <Nav.Link as={NavLink} to="/signup" style={{color: "#000", backgroundColor : "#EEE", borderRadius: 10}}>Register</Nav.Link>
+                        {/* <Nav.Link as={NavLink} to="/profile" style={{color: "#EEEEEE"}}>Avatar</Nav.Link> */}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
