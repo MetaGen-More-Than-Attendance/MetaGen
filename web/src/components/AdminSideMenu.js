9da8befb447c87764cd9edcd346c2ffdb50cbda5
bayogluteoman <bayogluteoman@gmail.com>
2022-04-02 04:26:57 +0300

    add new items to admin side menu

--- web/src/components/AdminSideMenu.js
+++ web/src/components/AdminSideMenu.js
@@ -6,11 +6,10 @@ const AdminSideMenu = () => {
     const navigate = useNavigate();
     return (
         <ListGroup style={{ width: "13%", height: "80%", backgroundColor: "#222831", marginTop: 75, borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE", marginTop: 110 }} onClick={() => navigate("/admin/addTeacher")}>Add Teacher</ListGroup.Item>
+            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE", marginTop: 120 }} onClick={() => navigate("/admin/addTeacher")}>Add Teacher</ListGroup.Item>
             <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/admin/addStudent")}>Add Student</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} >xxxxx</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} >yyyyy</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} >zzzzz</ListGroup.Item>
+            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/admin/addLecture")}>Add Lecture</ListGroup.Item>
+            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/admin/addSemester")}>Add Semester</ListGroup.Item>
         </ListGroup>
     )
 }
