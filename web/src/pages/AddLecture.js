a1ff6e123b57dde1877da954c5617d11eefe6b17
bayogluteoman <bayogluteoman@gmail.com>
2022-04-02 05:16:57 +0300

    fix form inputs

--- web/src/pages/AddLecture.js
+++ web/src/pages/AddLecture.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Dropdown, DropdownButton, Form, FormControl, InputGroup } from 'react-bootstrap'
+import { Button, Dropdown, DropdownButton, Form, InputGroup } from 'react-bootstrap'
 import AdminSideMenu from '../components/AdminSideMenu'
 
 const AddLecture = () => {
@@ -15,20 +15,27 @@ const AddLecture = () => {
                         <Form.Control type="input" placeholder="Enter lecture name" />
                     </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '60%' }}>
-                        <Form.Label>Semester</Form.Label>
-                        <InputGroup className="mb-3">
-                            <DropdownButton
-                                variant="outline-secondary"
-                                title="Choose Semester"
-                                id="input-group-dropdown-1"
-                            >
-                                <Dropdown.Item href="#">Fall</Dropdown.Item>
-                                <Dropdown.Divider />
-                                <Dropdown.Item href="#">Spring</Dropdown.Item>
-                            </DropdownButton>
-                        </InputGroup>
-                    </Form.Group>
+                    <div style={{ display: 'flex', width: '60%', flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '60%' }}>
+                            <Form.Label>Teacher Name</Form.Label>
+                            <Form.Control type="input" placeholder="Search teacher name" style={{ width: '60%' }} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '60%' }}>
+                            <Form.Label>Semester</Form.Label>
+                            <InputGroup className="mb-3">
+                                <DropdownButton
+                                    variant="outline-secondary"
+                                    title="Choose Semester"
+                                    id="input-group-dropdown-1"
+                                >
+                                    <Dropdown.Item href="#">Fall</Dropdown.Item>
+                                    <Dropdown.Divider />
+                                    <Dropdown.Item href="#">Spring</Dropdown.Item>
+                                </DropdownButton>
+                            </InputGroup>
+                        </Form.Group>
+                    </div>
 
                     <Form.Group className="mb-3" controlId="formBasicEmail" style={{ width: '60%' }}>
                         <Form.Label>Lecture Code</Form.Label>
@@ -47,14 +54,12 @@ const AddLecture = () => {
                         </Form.Group>
                     </div>
 
-
                     <Button type="submit" style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5" }}>
                         Submit
                     </Button>
                 </Form>
-            </div>
-
-        </div>
+            </div >
+        </div >
     )
 }
 
