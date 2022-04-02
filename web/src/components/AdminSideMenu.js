c1e576d381e806685aca2a681d439502c98ebeec
bayogluteoman <bayogluteoman@gmail.com>
2022-04-02 17:19:19 +0300

    add dropdown button to admin side menu

--- web/src/components/AdminSideMenu.js
+++ web/src/components/AdminSideMenu.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import { ListGroup } from 'react-bootstrap'
+import { DropdownButton, ListGroup, ButtonGroup, Dropdown } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 
 const AdminSideMenu = () => {
     const navigate = useNavigate();
     return (
         <ListGroup style={{ width: "13%", height: "80%", backgroundColor: "#222831", marginTop: 75, borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE", marginTop: 120 }} onClick={() => navigate("/admin/addTeacher")}>Add Teacher</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/admin/addStudent")}>Add Student</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/admin/addLecture")}>Add Lecture</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/admin/addSemester")}>Add Semester</ListGroup.Item>
+            <DropdownButton action variant="dark" as={ButtonGroup} title="Teacher" id="bg-vertical-dropdown-2" className="mb-3" style={{ backgroundColor: "#222831", color: "#EEE", marginTop: 120 }}>
+                <Dropdown.Item eventKey="1" onClick={() => navigate("/admin/addTeacher")}>Add Teacher</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Display Teacher</Dropdown.Item>
+            </DropdownButton>
+
+            <DropdownButton action variant="dark" as={ButtonGroup} title="Student" id="bg-vertical-dropdown-2" className="mb-3" style={{ backgroundColor: "#222831", color: "#EEE" }}>
+                <Dropdown.Item eventKey="1" onClick={() => navigate("/admin/addStudent")}>Add Student</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Display Student</Dropdown.Item>
+            </DropdownButton>
+
+            <DropdownButton action variant="dark" as={ButtonGroup} title="Lecture" id="bg-vertical-dropdown-2" className="mb-3" style={{ backgroundColor: "#222831", color: "#EEE" }}>
+                <Dropdown.Item eventKey="1" onClick={() => navigate("/admin/addLecture")}>Add Lecture</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Display Lecture</Dropdown.Item>
+            </DropdownButton>
+
+            <DropdownButton action variant="dark" as={ButtonGroup} title="Semester" id="bg-vertical-dropdown-2" className="mb-3" style={{ backgroundColor: "#222831", color: "#EEE" }}>
+                <Dropdown.Item eventKey="1" onClick={() => navigate("/admin/addSemester")}>Add Semester</Dropdown.Item>
+                <Dropdown.Item eventKey="2">Display Semester</Dropdown.Item>
+            </DropdownButton>
+
         </ListGroup>
     )
 }
