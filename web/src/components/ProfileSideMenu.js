f6bb6f867972a885926eb897504128b7616132a9
bayogluteoman <bayogluteoman@gmail.com>
2022-04-02 04:05:02 +0300

    add new item to side menu

--- web/src/components/ProfileSideMenu.js
+++ web/src/components/ProfileSideMenu.js
@@ -7,11 +7,10 @@ const ProfileSideMenu = () => {
     const navigate = useNavigate();
     return (
         <ListGroup style={{ width: "13%", height: "80%", backgroundColor: "#222831", marginTop: 75, borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE", marginTop: 110 }} onClick={() => navigate("/profile")}>Profile</ListGroup.Item>
+            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE", marginTop: 140 }} onClick={() => navigate("/profile")}>Profile</ListGroup.Item>
             <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/profile/settings")}>Settings</ListGroup.Item>
             <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/profile/lectures")}>Lectures</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} >yyyyy</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} >zzzzz</ListGroup.Item>
+            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/profile/semester")}>Semester</ListGroup.Item>
         </ListGroup>
     )
 }
