75b0014f80b4e9513db388040c811a988d8ba139
bayogluteoman <bayogluteoman@gmail.com>
2022-04-07 20:07:31 +0300

    add Formik for input validation

--- web/src/components/AddUser.js
+++ web/src/components/AddUser.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Button, Form, Image } from 'react-bootstrap'
+import { Formik } from 'formik';
 
 const AddUser = () => {
 
@@ -8,53 +9,186 @@ const AddUser = () => {
 
     const changeHandler = (event) => {
         setSelectedFile(URL.createObjectURL(event.target.files[0]));
+        console.log(selectedFile);
         setIsFilePicked(true);
     };
 
+    const initialValues = {
+        name: "",
+        surname: "",
+        id: "",
+        department: "",
+        email: "",
+        password: "",
+        // image: ""
+    };
     return (
         <div>
-            <div style={{ display: 'flex', float: 'left ', width: '30%', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }} >
-                <Form.Group controlId="formFile" className="mr-5" style={{ width: '70%' }}>
-                    <Form.Control type="file" onChange={changeHandler} />
-                </Form.Group>
-                {isFilePicked && <Image src={selectedFile} roundedCircle={true} className="mt-5 mr-5" style={{ width: '70%' }} />}
-            </div>
+            <Formik
+                initialValues={initialValues}
+                validate={(values) => {
+                    const errors = {};
+                    if (!values.email) {
+                        errors.email = '*';
+                    } else if (
+                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                        errors.email = '*Invalid email address!';
+                    }
+                    if (!values.name) {
+                        errors.name = '*';
+                    }
+                    if (!values.surname) {
+                        errors.surname = '*';
+                    }
+                    if (!values.id) {
+                        errors.id = '*';
+                    }
+                    if (!values.department) {
+                        errors.department = '*';
+                    }
+                    if (!values.password) {
+                        errors.password = '*';
+                    }
+                    // if (!values.image) {
+                    //     errors.image = '*image';
+                    // }
 
-            <Form style={{ width: '70%', float: "right" }}>
-                <Form.Group className="mb-1" controlId="formBasicEmail" style={{ width: '60%' }}>
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="input" placeholder="Enter teacher name" />
-                </Form.Group>
+                    return errors;
+                }}
 
-                <Form.Group className="mb-1" controlId="formBasicEmail" style={{ width: '60%' }}>
-                    <Form.Label>Surname</Form.Label>
-                    <Form.Control type="input" placeholder="Enter teacher surname" />
-                </Form.Group>
+                onSubmit={(values, { setSubmitting }) => {
+                    console.log(values);
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                    isSubmitting,
+                }) => (
+                    <div>
+                        <div style={{ display: 'flex', float: 'left ', width: '30%', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }} >
+                            <Form.Group controlId="formFile" className="mr-5" style={{ width: '70%' }}>
+                                <Form.Control type="file" name="image" onChange={changeHandler} />
+                                {/* {errors.image && touched.image && (<div style={{ color: 'red', marginRight: 5 }}>{errors.image}</div>)} */}
+                            </Form.Group>
+                            {isFilePicked && <Image src={selectedFile} roundedCircle={true} className="mt-5 mr-5" style={{ width: '70%' }} />}
+                        </div>
 
-                <Form.Group className="mb-1" controlId="formBasicEmail" style={{ width: '60%' }}>
-                    <Form.Label>Identity Number</Form.Label>
-                    <Form.Control type="input" placeholder="Enter teacher identity number" />
-                </Form.Group>
 
-                <Form.Group className="mb-1" controlId="formBasicEmail" style={{ width: '60%' }}>
-                    <Form.Label>Department</Form.Label>
-                    <Form.Control type="input" placeholder="Enter teacher department" />
-                </Form.Group>
+                        <Form onSubmit={handleSubmit} style={{ width: '70%', float: "right" }} >
+                            <Form.Group className="mb-1" style={{ width: '60%' }}>
 
-                <Form.Group className="mb-1" controlId="formBasicEmail" style={{ width: '60%' }}>
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+                                <div style={{ display: 'flex' }}>
+                                    {errors.name && touched.name && (<div style={{ color: 'red', marginRight: 5 }}>{errors.name}</div>)}
+                                    <Form.Label>Name</Form.Label>
+                                </div>
 
-                <Form.Group className="mb-1" controlId="formBasicPassword" style={{ width: '60%' }}>
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                                <Form.Control
+                                    type="input"
+                                    name='name'
+                                    value={values.name}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Enter teacher name" />
+                            </Form.Group>
 
-                <Button type="submit" style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5" }}>
-                    Submit
-                </Button>
-            </Form>
+                            <Form.Group className="mb-1" style={{ width: '60%' }}>
+
+                                <div style={{ display: 'flex' }}>
+                                    {errors.surname && touched.surname && (<div style={{ color: 'red', marginRight: 5 }}>{errors.surname}</div>)}
+                                    <Form.Label>Surname</Form.Label>
+                                </div>
+
+                                <Form.Control
+                                    type="input"
+                                    name='surname'
+                                    value={values.surname}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Enter teacher surname" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-1" style={{ width: '60%' }}>
+
+                                <div style={{ display: 'flex' }}>
+                                    {errors.id && touched.id && (<div style={{ color: 'red', marginRight: 5 }}>{errors.id}</div>)}
+                                    <Form.Label>Identity Number</Form.Label>
+                                </div>
+
+                                <Form.Control
+                                    type="input"
+                                    name='id'
+                                    value={values.id}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Enter teacher identity number" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-1" style={{ width: '60%' }}>
+
+                                <div style={{ display: 'flex' }}>
+                                    {errors.department && touched.department && (<div style={{ color: 'red', marginRight: 5 }}>{errors.department}</div>)}
+                                    <Form.Label>Department</Form.Label>
+                                </div>
+
+                                <Form.Control
+                                    type="input"
+                                    name="department"
+                                    value={values.department}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Enter teacher department" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-1" style={{ width: '60%' }}>
+
+                                <div style={{ display: 'flex' }}>
+                                    {errors.email && touched.email && (<div style={{ color: 'red', marginRight: 5 }}>{errors.email}</div>)}
+                                    <Form.Label>Email address</Form.Label>
+                                </div>
+
+                                <Form.Control
+                                    type="email"
+                                    name='email'
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Enter email" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-1" style={{ width: '60%' }}>
+
+                                <div style={{ display: 'flex' }}>
+                                    {errors.password && touched.password && (<div style={{ color: 'red', marginRight: 5 }}>{errors.password}</div>)}
+                                    <Form.Label>Password</Form.Label>
+                                </div>
+
+                                <Form.Control
+                                    type="password"
+                                    name='password'
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="Password" />
+                            </Form.Group>
+
+                            <Button type="submit" disabled={isSubmitting} style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5" }}>
+                                Submit
+                            </Button>
+
+                        </Form>
+                    </div>
+                )}
+            </Formik>
         </div>
     )
 }
