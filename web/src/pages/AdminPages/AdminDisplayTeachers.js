62a27d7cf278bd133a907dd701135705d0139cd5
bayogluteoman <bayogluteoman@gmail.com>
2022-04-10 22:34:35 +0300

    feat: add formik to control edit user teacher form

--- web/src/pages/AdminPages/AdminDisplayTeachers.js
+++ web/src/pages/AdminPages/AdminDisplayTeachers.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Table, Button, Modal, Form, Image } from 'react-bootstrap'
 import AdminSideMenu from '../../components/SideMenus/AdminSideMenu'
 import avatar from '../../images/logo512.png'
+import { Formik } from "formik";
 
 const AdminDisplayTeachers = () => {
   const [show, setShow] = useState(false);
@@ -15,6 +16,14 @@ const AdminDisplayTeachers = () => {
   const changeHandler = (event) => {
     setSelectedFile(URL.createObjectURL(event.target.files[0]));
     setIsFilePicked(true);
+  };
+
+  const initialValues = {
+    name: "",
+    surname: "",
+    identityNumber: "",
+    department: "",
+    email: "",
   };
 
   return (
@@ -65,66 +74,169 @@ const AdminDisplayTeachers = () => {
             <Modal.Title>Edit Teacher</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Form>
-              <Form.Group className='mb-3'>
-                <Form.Label>Image</Form.Label>
-                <Form.Control type="file" onChange={changeHandler} />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Name</Form.Label>
-                <Form.Control
-                  type="input"
-                  placeholder="Calculus 1"
-                  autoFocus
-                />
-              </Form.Group>
+            <Formik
+              initialValues={initialValues}
+              validate={(values) => {
+                const errors = {};
+                if (!values.email) {
+                  errors.email = "*";
+                } else if (
+                  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                ) {
+                  errors.email = "*Invalid email address!";
+                }
+                if (!values.name) {
+                  errors.name = "*";
+                }
+                if (!values.surname) {
+                  errors.surname = "*";
+                }
+                if (!values.identityNumber) {
+                  errors.identityNumber = "*";
+                }
+                if (!values.department) {
+                  errors.department = "*";
+                }
 
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Surname</Form.Label>
-                <Form.Control
-                  type="input"
-                  placeholder="Teoman Bayoglu"
-                  autoFocus
-                />
-              </Form.Group>
+                return errors;
+              }}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                handleSubmit,
+                isSubmitting,
+              }) => (
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group className='mb-3'>
+                    <Form.Label>Image</Form.Label>
+                    <Form.Control type="file" onChange={changeHandler} />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <div style={{ display: "flex" }}>
+                      {errors.name && touched.name && (
+                        <div style={{ color: "red", marginRight: 5 }}>
+                          {errors.name}
+                        </div>
+                      )}
+                      <Form.Label>Name</Form.Label>
+                    </div>
 
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Identity Number</Form.Label>
-                <Form.Control
-                  type="input"
-                  placeholder="Fall"
-                  autoFocus
-                />
-              </Form.Group>
+                    <Form.Control
+                      type="input"
+                      name="name"
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="Calculus 1"
+                      autoFocus
+                    />
+                  </Form.Group>
 
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Department</Form.Label>
-                <Form.Control
-                  type="input"
-                  placeholder="BIM 344"
-                  autoFocus
-                />
-              </Form.Group>
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <div style={{ display: "flex" }}>
+                      {errors.surname && touched.surname && (
+                        <div style={{ color: "red", marginRight: 5 }}>
+                          {errors.surname}
+                        </div>
+                      )}
+                      <Form.Label>Surname</Form.Label>
+                    </div>
 
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder=""
-                  autoFocus
-                />
-              </Form.Group>
-            </Form>
+                    <Form.Control
+                      type="input"
+                      name="surname"
+                      value={values.surname}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="Teoman Bayoğlu"
+                      autoFocus
+                    />
+                  </Form.Group>
 
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <div style={{ display: "flex" }}>
+                      {errors.identityNumber && touched.identityNumber && (
+                        <div style={{ color: "red", marginRight: 5 }}>
+                          {errors.identityNumber}
+                        </div>
+                      )}
+                      <Form.Label>Identity Number</Form.Label>
+                    </div>
+
+                    <Form.Control
+                      type="input"
+                      name="identityNumber"
+                      value={values.identityNumber}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="21232"
+                      autofocus
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <div style={{ display: "flex" }}>
+                      {errors.department && touched.department && (
+                        <div style={{ color: "red", marginRight: 5 }}>
+                          {errors.department}
+                        </div>
+                      )}
+                      <Form.Label>Department</Form.Label>
+                    </div>
+
+                    <Form.Control
+                      type="input"
+                      name="department"
+                      value={values.department}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="BIM 344"
+                      autoFocus
+                    />
+                  </Form.Group>
+
+                  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <div style={{ display: "flex" }}>
+                      {errors.email && touched.email && (
+                        <div style={{ color: "red", marginRight: 5 }}>
+                          {errors.email}
+                        </div>
+                      )}
+                      <Form.Label>Email address</Form.Label>
+                    </div>
+
+                    <Form.Control
+                      type="email"
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      placeholder="test@test.com"
+                      autofocus
+                    />
+                  </Form.Group>
+
+                  <Button
+                    type="submit"
+                    disabled={isSubmitting}
+                    style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5" }}
+                  >
+                    Submit
+                  </Button>
+                </Form>
+              )}
+            </Formik>
           </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
         </Modal>
       </div>
     </div>
