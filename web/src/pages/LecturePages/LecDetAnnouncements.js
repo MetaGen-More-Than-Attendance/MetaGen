faec32223125a7c4a5672852f9bded8b8dd063eb
bayogluteoman <bayogluteoman@gmail.com>
2022-04-23 01:39:05 +0300

    fix: fix responsive design

--- web/src/pages/LecturePages/LecDetAnnouncements.js
+++ web/src/pages/LecturePages/LecDetAnnouncements.js
@@ -3,8 +3,11 @@ import SideMenu from '../../components/SideMenus/SideMenu'
 
 const LecDetAnnouncements = () => {
   return (
-    <div style={{ height: "80vh" }}>
+    <div style={{ height: "80vh", display: 'flex' }}>
       <SideMenu />
+      <div style={{ marginLeft: 15, width: '100%', marginRight: 15, marginTop: 15}}>
+          <h1>Announcements</h1>
+      </div>
     </div>
   )
 }
