a78dd02c60865c35fa196f29f5f85d957cbfd2da
bayogluteoman <bayogluteoman@gmail.com>
2022-04-23 02:42:47 +0300

    fix: fix responsive design of lecture pages

--- web/src/components/Cards/LectureCard.js
+++ web/src/components/Cards/LectureCard.js
@@ -19,16 +19,16 @@ const LectureCard = ({lectureName, lectureInfo}) => {
 
     return (
         <div>
-            <Card style={{ width: '18rem', margin: '2rem', borderWidth: 1, borderColor: "#393E46", borderRadius: 20, boxShadow: "1px 1px 1px 1px #393E46" }}>
-                <Card.Img variant="top" src={img} style={{ height: '7rem', borderTopLeftRadius: 20, borderTopRightRadius: 20 }} />
+            <Card style={{ width: '80%', height:'100%', margin: '2rem', borderWidth: 1, borderColor: "#393E46", borderRadius: 20, boxShadow: "1px 1px 1px 1px #393E46" }}>
+                <Card.Img variant="top" src={img} style={{ height: '10%', borderTopLeftRadius: 20, borderTopRightRadius: 20 }} />
                 <Card.Body>
                     <Card.Title style={{ display: "flex", justifyContent: "center" }}>{lectureName}</Card.Title>
                     <Card.Text>
                         {lectureInfo}
                     </Card.Text>
                     <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <Button style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5" }} onClick={() => navigate("/lectureDetails")} >Details</Button>
-                        <Button style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5" }} onClick={handleShow} >
+                        <Button style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5", color:"#222831", fontWeight:"bold", width: '30%', }} onClick={() => navigate("/lectureDetails")} >Details</Button>
+                        <Button style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5", color:"#222831", fontWeight:"bold", width: '60%', }} onClick={handleShow} >
                             Generate Qr Code
                         </Button>
                     </div>
