5583e81b4396ff339aa1ad1c24f4c7b35b1a8fb6
bayogluteoman <bayogluteoman@gmail.com>
2022-04-30 15:28:37 +0300

    fix: make full screen for page

--- web/src/pages/NotFoundPage/NotFoundPage.js
+++ web/src/pages/NotFoundPage/NotFoundPage.js
@@ -10,10 +10,10 @@ const NotFoundPage = () => {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '90vh', backgroundColor: 'black', marginTop: -14 }}>
+    <div style={{ position:'absolute', top:0, right: 0, left:0, width:'"100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: 'black',}}>
       <h1 style={{ color: 'red', fontSize: 95 }}>404</h1>
-      <h1 style={{ color: 'red', fontSize: 45 }}>Page Not Found !</h1>
-      <h3 style={{ color: 'white', padding: 10 }} >Sorry we can't find what you are looking for 'cuz its so dark in here</h3>
+      <h1 style={{ color: 'red', fontSize: 55 }}>Page Not Found !</h1>
+      <h3 style={{ color: 'white', padding:10 }} >Sorry we can't find what you are looking for 'cuz its so dark in here</h3>
       <Button variant="outline-danger" onClick={handleClick} >Home</Button>
     </div>
   )
