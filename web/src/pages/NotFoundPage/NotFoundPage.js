3c7d5880c0cc82fd598f88be16b5f0e8b20cbc15
bayogluteoman <bayogluteoman@gmail.com>
2022-04-30 04:14:28 +0300

    fix: add basic design to not found page

--- web/src/pages/NotFoundPage/NotFoundPage.js
+++ web/src/pages/NotFoundPage/NotFoundPage.js
@@ -1,6 +1,21 @@
+import { Button } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+
 const NotFoundPage = () => {
+
+  const navigate = useNavigate();
+  
+  const handleClick =  () => {
+    navigate("/home")
+  }
+
   return (
-    <div></div>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '90vh', backgroundColor: 'black', marginTop: -14 }}>
+      <h1 style={{ color: 'red', fontSize: 95 }}>404</h1>
+      <h1 style={{ color: 'red', fontSize: 45 }}>Page Not Found !</h1>
+      <h3 style={{ color: 'white', padding: 10 }} >Sorry we can't find what you are looking for 'cuz its so dark in here</h3>
+      <Button variant="outline-danger" onClick={handleClick} >Home</Button>
+    </div>
   )
 }
 
