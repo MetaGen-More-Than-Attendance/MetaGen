0f5362db551a30b4653da01571d365385240eb37
bayogluteoman <bayogluteoman@gmail.com>
2022-05-15 23:44:00 +0300

    feat: add table to see department data

--- web/src/pages/AdminPages/AdminDisplayDepartment.js
+++ web/src/pages/AdminPages/AdminDisplayDepartment.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Table } from 'react-bootstrap'
 import AdminSideMenu from '../../components/SideMenus/AdminSideMenu'
 
 const AdminDisplayDepartment = () => {
@@ -8,6 +9,20 @@ const AdminDisplayDepartment = () => {
             <div style={{ marginLeft: 15, width: '100%', marginRight: 15 }}>
                 <h1 style={{ color: '#9C9FA3' }}>Display All Departments</h1>
                 <hr style={{ color: '#222831', width: '97%' }} />
+                <Table striped bordered hover responsive="md">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Department</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr >
+                            <td></td>
+                            <td colSpan={2}>Computer</td>
+                        </tr>                                              
+                    </tbody>
+                </Table>
             </div>
         </div>
     )
