f1d038b883b0ab29414f5571af4a849e694cf2dc
bayogluteoman <bayogluteoman@gmail.com>
2022-05-20 20:35:21 +0300

    fix: add dropdown button for attendance menu

--- web/src/components/SideMenus/SideMenu.js
+++ web/src/components/SideMenus/SideMenu.js
@@ -1,15 +1,17 @@
 import React from 'react'
-import { ListGroup } from 'react-bootstrap'
+import { Dropdown, DropdownButton, ListGroup, ButtonGroup } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
 
 const SideMenu = () => {
     const navigate = useNavigate();
-    
+
     return (
         <ListGroup style={{ height: "80%", backgroundColor: "#222831", marginTop: 75, borderTopRightRadius: 10, borderBottomRightRadius: 10 }}>
             <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE", marginTop: 110 }} onClick={() => navigate("/lectureDetails")}>Home</ListGroup.Item>
             <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/lectureDetails/people")}>People</ListGroup.Item>
-            <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/lectureDetails/attendance")}>Attendance</ListGroup.Item>
+            <DropdownButton action variant="dark" as={ButtonGroup} title="Attendance" id="bg-vertical-dropdown-2" style={{ backgroundColor: "#222831", color: "#EEE" }}>
+                <Dropdown.Item eventKey="1" onClick={() => navigate("/lectureDetails/attendance")}>Specific Date</Dropdown.Item>
+            </DropdownButton>
             <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/lectureDetails/calendar")}>Calendar</ListGroup.Item>
             <ListGroup.Item action variant="dark" style={{ backgroundColor: "#222831", color: "#EEE" }} onClick={() => navigate("/lectureDetails/announcements")}>Announcements</ListGroup.Item>
         </ListGroup>
