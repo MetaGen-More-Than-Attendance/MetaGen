31a8142dc08a7d6165e10ab0bdc290888679fb95
bayogluteoman <bayogluteoman@gmail.com>
2022-05-22 22:04:33 +0300

    fix: add background image to home page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import AdminSideMenu from '../../components/SideMenus/AdminSideMenu'
+import background from '../../images/homeBg.jpg';
 
 const HomePage = ({ isAdmin }) => {
     return (
-        <div style={{ height: "80vh", display: "flex" }}>
-            {isAdmin === true ? <AdminSideMenu /> : null}
+        <div style={{ height: "88.6vh", display: "flex" }}>
+            {isAdmin === true ?
+                <AdminSideMenu />
+                :
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    textAlign: 'center',
+                    backgroundImage: `url(${background})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center center',
+                    backgroundRepeat: 'no-repeat',
+                    width: '100%'
+                }}>
+                    <h1 style={{ color: "#00ADB5",   }}>WELCOME TO METAGEN</h1>
+                    <p style={{ color: "#00ADB5",   }}>More than attendance</p>
+                </div>
+
+            }
         </div>
     )
 }
