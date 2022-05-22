6bfd1bad798995728a2e65a4d79757a2a175ceb0
bayogluteoman <bayogluteoman@gmail.com>
2022-05-22 19:20:24 +0300

    feat: display lecture's student in table thanks to API

--- web/src/pages/LecturePages/LecDetPeople.js
+++ web/src/pages/LecturePages/LecDetPeople.js
@@ -1,76 +1,52 @@
-import React from 'react'
-import { Table } from 'react-bootstrap'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Image, Table } from 'react-bootstrap'
+
 import SideMenu from '../../components/SideMenus/SideMenu'
+import { fetchLectureStudents } from '../../redux/features/lecture/lectureSlice';
 
 const LecDetPeople = () => {
+  const lectureId = localStorage.getItem("lectureId");
+
+  const dispatch = useDispatch();
+
+  const lectureStudents = useSelector((state) => state.lectures.entities);
+
+  useEffect(() => {
+    dispatch(fetchLectureStudents(lectureId));
+  }, [dispatch, lectureId]);
+
+
   return (
     <div style={{ height: "80vh", display: "flex" }}>
       <SideMenu />
       <div style={{ marginLeft: 15, width: '100%', marginRight: 15 }}>
-        <h1 style={{ color: '#9C9FA3' }}>{localStorage.getItem("lectureName")}</h1>
+        <h1 style={{ color: '#9C9FA3' }}>{localStorage.getItem("lectureName")}'s Students</h1>
         <hr style={{ color: '#222831', width: '100%' }} />
         <Table striped bordered hover responsive="md">
           <thead>
             <tr>
               <th></th>
+              <th style={{display: 'flex', justifyContent: 'center'}}>Image</th>
               <th>First Name</th>
               <th>Last Name</th>
-              <th>Username</th>
+              <th>Department</th>
+              <th style={{display: 'flex', justifyContent: 'center'}}>Email</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Larry</td>
-              <td>Bird</td>
-              <td>@twitter</td>
-            </tr> <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Larry</td>
-              <td>Bird</td>
-              <td>@twitter</td>
-            </tr> <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Larry</td>
-              <td>Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {lectureStudents.map((student) => {
+              return (
+                <tr>
+                  <td>{student.studentId}</td>
+                  <td style={{ display: 'flex', justifyContent: 'center' }}><Image src={`data:image/jpeg;base64,${student.photo}`} alt="?" rounded={true} style={{ width: '2rem' }} /></td>
+                  <td>{student.userName}</td>
+                  <td>{student.userSurname}</td>
+                  <td>{student.departmentName}</td>
+                  <td style={{display: 'flex', justifyContent: 'center'}}><Button style={{ backgroundColor: "#00ADB5", borderColor: "#00ADB5" }} onClick={() => window.location = `mailto:${student.userMail}`}>Contact</Button></td>
+                </tr>
+              )
+            })}
           </tbody>
         </Table>
       </div>
