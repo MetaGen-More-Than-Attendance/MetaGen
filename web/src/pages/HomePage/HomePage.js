4812fb23368973087c1aaccc37f3a1ac15ea47ae
bayogluteoman <bayogluteoman@gmail.com>
2022-05-23 03:08:50 +0300

    fix: add background to admin dashboard

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -4,9 +4,24 @@ import background from '../../images/homeBg2.jpg';
 
 const HomePage = ({ isAdmin }) => {
     return (
-        <div style={{ height: "88.6vh", display: "flex" }}>
+        <div style={{
+            height: "88.6vh",
+            display: "flex",
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+        }}>
             {isAdmin === true ?
-                <AdminSideMenu />
+                <div 
+                style={{
+                    display: "flex",
+                    width: "100%",
+                }}
+                >
+                    <AdminSideMenu />
+                    <h1 style={{display:'flex', height:'70%', color: "#00ADB5", flexDirection: 'column', justifyContent:'center', textAlign:'center', width:'100%'}}>WELCOME TO ADMIN DASHBOARD 🎉</h1>
+                    </div>
                 :
                 <div style={{
                     display: 'flex',
@@ -19,8 +34,8 @@ const HomePage = ({ isAdmin }) => {
                     backgroundRepeat: 'no-repeat',
                     width: '100%'
                 }}>
-                    <h1 style={{ color: "#00ADB5",   }}>WELCOME TO METAGEN</h1>
-                    <p style={{ color: "#00ADB5",   }}>More than attendance</p>
+                    <h1 style={{ color: "#00ADB5", }}>WELCOME TO METAGEN</h1>
+                    <p style={{ color: "#00ADB5", }}>More than attendance</p>
                 </div>
 
             }
